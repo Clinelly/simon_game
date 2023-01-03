048279edc407eb2458d0796ff45ac57be6aedf36
Clinelly <swall289@gmail.com>
2023-01-03 13:30:33 +0000

    Add additional gameplay functions & tests.

--- script/game.js
+++ script/game.js
@@ -3,6 +3,7 @@ let game = {
     currentGame: [],
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"],
+    turnNumber: 0,
 }
 
 function newGame() {
@@ -10,10 +11,35 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = {game, newGame, showScore};
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = {game, newGame, showScore, addTurn, lightsOn, showTurns};
